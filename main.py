16f85528930d04f3345a98b1404c5383566ca75f
Matthew <cyrilmartinmj@gmail.com>
2026-01-04 18:50:05 -0500

    created main

---
+++ main.py
@@ -0,0 +1,6 @@
+## create window to select type of server and region
+## close that window and create new window to show list of servers based on previous selection
+## close that window and create a new window to show details of selected server
+##   if that server is down, show option to check server
+
+## server checker will use the blizzard API to check server status, and create a notification when server comes up
